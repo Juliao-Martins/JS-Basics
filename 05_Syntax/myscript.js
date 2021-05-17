266339456589f812c673bf0a2cbd9a739e5122f9
Juliao-from-EastTimor <juliaodejesusmartins@gmail.com>
2021-05-17 12:43:40 -0700

    Learn JavaScript Syntax.

---
+++ 05_Syntax/myscript.js
@@ -0,0 +1,51 @@
+var x, y, z;	// declare variables
+x = 5;			// assign the value 5 to x
+y = 6;			// assign the value 6 to y
+z = x + y;		// assign the sum of x and y to z
+
+// or you can except
+// variable x store value 5
+// variable y store value 6
+// variable z store value 11 (5+6)
+
+// JavaScript have two values are
+// fixed values are called literals
+// variable values are called variables
+
+// fixed value like "john doe" or 2014
+
+// variable use to store data values like text or number
+// like string writting with single or double quotes
+// or numbers writting without quotes
+var name = "john doe";
+var pi = 3.14;
+
+// JavaScript have technically to give variable name
+// lower Camel Case
+// JavaScript interpreter not VAR or Var but var keyword to declare a variable
+
+// variable name with lower camel case
+// one var keyword many variable with lower camel case
+var firstName = "Juliao",
+lastName = " Martins",
+myAge = 20,
+myHobby = "Game";
+
+// JavaScript are not use technically give name for variable like
+// hyphen: first-name, last-name in javascript reversed subtraction
+// underscore: first_name, last_name
+// upper camel case: FirstName, LastName
+// but javascript programmer recommended lower camel case
+
+/*
+
+	declaring a variable
+	var keyword, variableName, equal sign, value;
+
+	var x = 10;
+	
+	assignment operator (=) equal sign to assign value to variable
+
+	= 	is equal sign not equal to (comparison operate)
+	== 	is equal to to compare the value
+*/
