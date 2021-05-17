26c07e73a2bd0616696d34e1f0426c9ab7845645
Juliao-from-EastTimor <juliaodejesusmartins@gmail.com>
2021-05-17 11:03:50 -0700

    How to output an JavaScript code in browser.

---
+++ 03_Output/myscript.js
@@ -0,0 +1,22 @@
+/*
+this is multi-line comment JS will be ignored (not execute in browser)
+
+you can use the keyword to display js code in output
+
+1. writting in html element content with 'innerHTML=""'
+2. writting in inside html with 'document.write()'
+3. writting in browser console with 'console.log()'
+4. writting in alert box with 'window.alert()'
+
+*/
+
+document.getElementById("demo").innerHTML = "My First JavaScript!";
+
+// document.write(5 + 6);
+// document.write("Hello, Again!");
+
+console.log(5 + 6);
+console.log("Hello, Juliao");
+
+// window.alert("Learn JavaScript!");
+// alert("JavaScript is the best!");
