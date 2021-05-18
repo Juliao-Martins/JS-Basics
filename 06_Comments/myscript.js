f601e13dd42d8c1ff8b137226fd2eab594a9e8be
Juliao-from-EastTimor <juliaodejesusmartins@gmail.com>
2021-05-17 20:40:37 -0700

    Learn JavaScript Comments.

---
+++ 06_Comments/myscript.js
@@ -0,0 +1,28 @@
+// single comment start with double slash
+// JavaScript will ignored (not execute by browser)
+// comment cant use prevent and explain your code
+
+// change my HTML element content with id="myH" to "Hello, Web Developers!"
+document.getElementById("myH").innerHTML = "Hello, Web Developers!";
+
+// change my HTML element content with id="myP" to "HTML, CSS and JavaScript" 
+// document.getElementById("myP").innerHTML = "HTML, CSS and JavaScript";
+
+/*
+this is multi-line comment
+
+you can give to multi-line 
+
+and javascript will ignored
+there in browser execute
+*/
+
+/*
+change my HTML element style (css)
+font-size to 36px,
+color to orange,
+font-style oblique
+for id="myP"
+*/
+
+document.getElementById("myP").setAttribute("style","font-size: 36px; color: orange; font-style: oblique;");
